86ede79793e76344d552b1851e7876e8276156b5
youheihosoda <youhe0122@gmail.com>
2021-07-19 14:36:33 +0000

    rspec_user_model

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
-  factory :user do
-    email { Faker::Internet.email }
-    password { 'password' }
-    password_confirmation { 'password' }
+  factory :user do #factory :testuser, class: User do のようにクラスを明示すればモデル名以外のデータも作れます。
+    name { "test" }
+    email { "test@test.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, "モデルに関するテスト", type: :model do
-	  describe 'バリデーション' do
-	    it "有効なユーザー情報は保存されるか" do
-	      expect(FactoryBot.build(:user)).to be_valid
-	    end
-	  end
-	  context "空白のバリデーションチェック" do
-	    it "nameが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
-	      user.name = ''
-	      is_expected.to eq false;
-	    end
-    end
+describe User do
+ describe '#create' do
+
+   # 1
+   it "nickname、email、passwordとpassword_confirmationが存在すれば登録できること" do
+     user = build(:user)
+     expect(user).to be_valid
+   end
 end
-
-
+end
 
