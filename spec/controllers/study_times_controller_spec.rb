dce8c7f4ddead05053153de600b2a3790a088a52
youheihosoda <youhe0122@gmail.com>
2021-07-31 14:30:19 +0000

    rsepec_controller_study_time

--- spec/controllers/study_times_controller_spec.rb
+++ spec/controllers/study_times_controller_spec.rb
@@ -31,4 +31,34 @@ RSpec.describe Public::StudyTimesController, type: :controller do
       expect(response).to be_success
     end
   end
+  
+  describe "CRUD機能のテスト" do
+    before do
+      @user = FactoryBot.create(:user)
+      @study_time = @user.study_times.create(study_method: "test")
+    end
+    
+    it "学習開始時刻を取得できるか" do
+      sign_in @user
+      expect {post :start_time, 
+      params: {study_time: {study_method: "test",user_id: 1, start_time: 1624180436, end_time: 1624180438}
+      }
+      }.to change(@user.study_times, :count).by(1)
+    end
+    
+    it "学習終了時刻を取得できるか" do
+      sign_in @user
+        study_time_params = {study_method: "test",user_id: 1, start_time: 1624180436, end_time: 1627712035}
+        patch :stop_time, params: {id: @study_time.id, study_time: study_time_params}
+        @study_time.reload
+        expect(@study_time.end_time).to be >= 1624180436
+    end
+    
+    it "学習時間取得後に投稿画面に遷移するか" do
+      sign_in @user
+      study_time_params = {study_method: "test",user_id: 1, start_time: 1624180436, end_time: 1624180438}
+      patch :stop_time, params: {id: @study_time.id, study_time: study_time_params}
+      expect(response).to redirect_to "/public/study_times/1/edit?study_time_id=1"
+    end
+  end
 end
